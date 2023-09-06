89dca43a377bb6feab4a710fecd9ff61792f0d79
danielbernalb <danielbernalb@gmail.com>
2023-09-05 22:43:45 -0500

    Por defecto Logging interval = 10 segundos

--- src/DataProvider.h
+++ src/DataProvider.h
@@ -71,7 +71,7 @@ class DataProvider: public IProviderCallbacks {
     int _numberOfSamplePacketsToDownload = 0;
 
     SampleConfig _sampleConfig;
-    uint64_t _historyIntervalMilliSeconds = 600000; // = 10 minutes
+    uint64_t _historyIntervalMilliSeconds = 10000; // = 10 seconds
     uint64_t _latestHistoryTimeStamp = 0;
     uint64_t _latestHistoryTimeStampAtDownloadStart = 0;
     IWifiLibraryWrapper* _pWifiLibaray;
